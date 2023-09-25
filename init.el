e671afcc4b81a973ec9c6460cabf639a727d5a44
ArcanusNEO <admin@transcliff.top>
2023-09-25 17:00:17 +0800

    Basic conf

---
+++ init.el
@@ -0,0 +1,41 @@
+(delete-selection-mode 1)
+(electric-indent-mode 1)
+(electric-pair-mode 1)
+(global-auto-revert-mode 1)
+(global-display-line-numbers-mode 1)
+(global-prettify-symbols-mode 1)
+(global-subword-mode 1)
+(menu-bar-mode 0)
+
+(put 'dired-find-alternate-file 'disabled nil)
+(put 'downcase-region 'disabled nil)
+(put 'erase-buffer 'disabled nil)
+(put 'narrow-to-page 'disabled nil)
+(put 'narrow-to-region 'disabled nil)
+(put 'scroll-left 'disabled nil)
+(put 'upcase-region 'disabled nil)
+
+(set-default-coding-systems 'utf-8)
+(set-keyboard-coding-system 'utf-8-unix)
+(set-language-environment 'utf-8)
+(set-terminal-coding-system 'utf-8-unix)
+
+(setq auto-save-default nil)
+(setq bookmark-save-flag nil)
+(setq create-lockfiles nil)
+(setq make-backup-files nil)
+
+(setq-default indent-tabs-mode nil)
+(setq-default tab-always-indent 'complete)
+(setq-default tab-width 2)
+(setq-default word-wrap t)
+(setq line-move-visual nil)
+
+(setq inhibit-startup-message t)
+(setq search-whitespace-regexp "[-_ \t\n]+")
+(defalias 'yes-or-no-p 'y-or-n-p)
+(setq use-short-answers t)
+
+(setq x-select-enable-clipboard t)
+(setq x-select-enable-primary t)
+

3b91af5e91e14593fbe319174f00475fcbffe91c
ArcanusNEO <admin@transcliff.top>
2023-09-26 01:50:50 +0800

    X11 clipboard fix

--- init.el
+++ init.el
@@ -36,6 +36,16 @@
 (defalias 'yes-or-no-p 'y-or-n-p)
 (setq use-short-answers t)
 
-(setq x-select-enable-clipboard t)
-(setq x-select-enable-primary t)
-
+(unless window-system
+  (when (getenv "DISPLAY")
+    (defun xclip-cut-function (text &optional push)
+      (with-temp-buffer
+	      (insert text)
+	      (call-process-region (point-min) (point-max) "xclip" nil 0 nil "-i" "-selection" "clipboard")))
+    (defun xclip-paste-function()
+      (let ((xclip-output (shell-command-to-string "xclip -o -selection clipboard")))
+	      (unless (string= (car kill-ring) xclip-output)
+	        xclip-output )))
+    (setq interprogram-cut-function 'xclip-cut-function)
+    (setq interprogram-paste-function 'xclip-paste-function)
+    ))

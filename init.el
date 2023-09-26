9de35e12df72cd2a3926886308454a564e843615
ArcanusNEO <admin@transcliff.top>
2023-09-27 02:32:25 +0800

    Removing clipboard syncing codes.

--- init.el
+++ init.el
@@ -38,18 +38,4 @@
 (defalias 'yes-or-no-p 'y-or-n-p)
 (setq use-short-answers t)
 
-(unless window-system
-  (when (getenv "DISPLAY")
-    (defun xclip-cut-function (text &optional push)
-      (with-temp-buffer
-	      (insert text)
-	      (call-process-region (point-min) (point-max) "xclip" nil 0 nil "-i" "-selection" "clipboard")))
-    (defun xclip-paste-function()
-      (let ((xclip-output (shell-command-to-string "xclip -o -selection clipboard")))
-	      (unless (string= (car kill-ring) xclip-output)
-	        xclip-output )))
-    (setq interprogram-cut-function 'xclip-cut-function)
-    (setq interprogram-paste-function 'xclip-paste-function)
-    ))
-
 (setq-default c-default-style "java")
